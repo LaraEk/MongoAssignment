5b3e39dfc2937fb27aa443ff98375b1a86966b69
Ekscursionist <larakek@gmail.com>
2018-09-18 09:34:52 -0400

    getting it to work with a site other than reddit!

--- server.js
+++ server.js
@@ -3,23 +3,39 @@ var request = require("request");
 
 console.log("[WIP] web scraper [WIP]");
 
-request("https://old.reddit.com/r/proplifting/", function(error, response, html) {
+// request("https://old.reddit.com/r/news/", function(error, response, html) {
 
+//     var $ = cheerio.load(html);
+//     var results = [];
+
+//     $("p.title").each(function(i, element) {
+
+//         var title = $(element).text();
+//         var link = $(element).children().attr("href");
+
+//         results.push({
+//             title: title,
+//             link: link
+//         });
+
+//     });
+
+//     console.log(results);
+
+// });
+
+request("https://www.scmp.com/frontpage/international", function(error, response, html) {
     var $ = cheerio.load(html);
     var results = [];
 
-    $("p.title").each(function(i, element) {
-
-        var title=$(element).text();
+    $("h3.lvl_25-title").each(function(i, element) {
+        var title = $(element).text();
         var link = $(element).children().attr("href");
 
         results.push({
             title: title,
             link: link
         });
-
     });
-
     console.log(results);
-
 });
